61511af2d90a4eff21ef739838ac6f301e865730
danfirsten <dan.first.4444@gmail.com>
2024-07-02 10:32:12 -0700

    Completed Items

--- online_marketplace/core/views.py
+++ online_marketplace/core/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
+
 # views.py typically contains functions or classes responsible for handling HTTP requests
 # and returning HTTP responses
 
 # Information about the browser, your API address, if it's a get or post request (or similar)
 # Has to be put in on all views that we use
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
