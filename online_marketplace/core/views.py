12aed59ec420dcd5ffdac410855e31a881ea55bc
danfirsten <dan.first.4444@gmail.com>
2024-07-02 12:04:13 -0700

    Sign Up Form

--- online_marketplace/core/views.py
+++ online_marketplace/core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
+
+from .forms import SignupForm
 
 # views.py typically contains functions or classes responsible for handling HTTP requests
 # and returning HTTP responses
@@ -17,3 +19,17 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
